e42767f2ba896c6ac6211966f788c62b812a0850
AlexandreCR <DevAlexandreCR@gmail.com>
2020-05-13 13:17:02 -0500

    configuracion inicial del proyecto

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAiyLcqZRO1A30lwTXNgfAcAriFFBhzs94",
+    authDomain: "aurumgamerco.firebaseapp.com",
+    databaseURL: "https://aurumgamerco.firebaseio.com",
+    projectId: "aurumgamerco",
+    storageBucket: "aurumgamerco.appspot.com",
+    messagingSenderId: "487789785710",
+    appId: "1:487789785710:web:cbff80a096b2b67dd02c7f",
+    measurementId: "G-5TVY9RB5T5"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAiyLcqZRO1A30lwTXNgfAcAriFFBhzs94",
+    authDomain: "aurumgamerco.firebaseapp.com",
+    databaseURL: "https://aurumgamerco.firebaseio.com",
+    projectId: "aurumgamerco",
+    storageBucket: "aurumgamerco.appspot.com",
+    messagingSenderId: "487789785710",
+    appId: "1:487789785710:web:cbff80a096b2b67dd02c7f",
+    measurementId: "G-5TVY9RB5T5"
+  }
 };
 
 /*
